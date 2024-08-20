3bf9aca62633dba0d49d4f078f4025a27d648af4
Aakash-prvt <akashpremiums03@gmail.com>
2024-08-20 23:02:54 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ try:
     DEV = 1664850827
     OWNER = config("OWNER", "5725206423")
     ffmpegcode = ["-preset superfast -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 12 -metadata title='𝙰𝚗𝚒𝚖𝚎_𝚆𝚊𝚛𝚒𝚘𝚛' -metadata author='𝙰𝚗𝚒𝚖𝚎_𝚆𝚊𝚛𝚒𝚘𝚛' -metadata:s:s title='𝙰𝚗𝚒𝚖𝚎_𝚆𝚊𝚛𝚒𝚘𝚛' -metadata:s:a title='𝙰𝚗𝚒𝚖𝚎_𝚆𝚊𝚛𝚒𝚘𝚛' -metadata:s:v title='𝙰𝚗𝚒𝚖𝚎_𝚆𝚊𝚛𝚒𝚘𝚛'"]
-    THUMBNAIL = config("THUMBNAIL", default="https://te.legra.ph/file/dbf430121d50d9324a3b7.jpg")
+    THUMB = config("THUMBNAIL", default="https://te.legra.ph/file/dbf430121d50d9324a3b7.jpg")
 except Exception as e:
     print("Environment vars Missing! Exiting App.")
     print(str(e))
